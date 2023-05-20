fd4df9726faa573d533434b6cdf0e5c5af5162c1
ctossato <53264012+ctossato@users.noreply.github.com>
2023-05-20 14:26:16 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,51 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Aluno (val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val inscritos = mutableListOf<Aluno>()
+
+    fun matricular(aluno: Aluno) {
+        inscritos.add(aluno)
+    }
+
+    override fun toString(): String {
+        return "Nome=$nome,  Conteudo=$conteudos, Nível=$nivel, Inscritos=$inscritos"
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val jose = Aluno("Jose Peçanha")
+    val miriam = Aluno("Miriam Leitão")
+
+    val cursoProcessoDesenvolvimentoSoftware = ConteudoEducacional("Processo de Desenvolvimento de Software", 120)
+    val cursoGit = ConteudoEducacional("Git")
+    val cursoJavaBasico = ConteudoEducacional("Java Básico", 180)
+    val cursoTestesUnitarioEmJava = ConteudoEducacional("Testes Unitarios em Java", 180)
+    val mentoriaPlantaoDuvidas = ConteudoEducacional("Mentoria de plantao de Duvidas", 90)
+    val cursoSpring = ConteudoEducacional("Spring Boot com Java", 160)
+    val cursoInjecaoDependencias = ConteudoEducacional("Injeção de dependencias com Dart", 90)
+
+    val javaBasicoFormacao = Formacao("Java Basico", listOf(cursoProcessoDesenvolvimentoSoftware, cursoGit, cursoJavaBasico, cursoTestesUnitarioEmJava, mentoriaPlantaoDuvidas), Nivel.BASICO)
+    val javaAvancadoFormacao = Formacao("Java Avancado", listOf(cursoTestesUnitarioEmJava, cursoSpring, cursoInjecaoDependencias, mentoriaPlantaoDuvidas), Nivel.DIFICIL)
+
+    println(javaBasicoFormacao)
+    println(javaAvancadoFormacao)
+
+    javaBasicoFormacao.matricular(jose)
+    javaAvancadoFormacao.matricular(miriam)
+
+    println(javaBasicoFormacao.inscritos)
+    println(javaAvancadoFormacao.inscritos)
+
+    println(javaBasicoFormacao)
+    println(javaAvancadoFormacao)
+
+
 }
